751e98ea46e5992f5bca93a18e72095a12ecf5c5
Your Name <you@example.com>
2024-04-25 15:28:28 +0000

    Add support for loading and printing the first .mp4 file in the selected template folder

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import time
+import json
 import os
 
 # CONSTANT
@@ -13,5 +13,19 @@ template="Minimalist Line-based Economic News Intro"
 
 template_folder = TEMPLATES_DIR + template + "/"
 mp4_file = [f for f in os.listdir(template_folder) if f.endswith('.mp4')][0]
-print(mp4_file)
 
+texts_config_file = template_folder + "texts.json"
+texts = {}
+if not os.path.exists(texts_config_file):
+    texts = {}
+    texts["texts"] = []
+    texts["texts"].append({"text": "Hello World", "position": [228.857, 99.2187]})
+    with open(texts_config_file, 'w') as outfile:
+        json.dump(texts, outfile)
+else:
+    with open(texts_config_file) as json_file:
+        texts = json.load(json_file)
+# 
+
+for text in texts["texts"]:
+    print(text["text"], text["position"])
