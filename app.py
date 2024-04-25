2cdc1f42bec45d2737f9290262dba0fd868f6197
Your Name <you@example.com>
2024-04-25 15:23:16 +0000

    Add support for loading and printing the first .mp4 file in the selected template folder

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-# sleep for 5 seconds
 import time
-time.sleep(5)
+import os
+
+# CONSTANT
+ASSETS_DIR = "/app/assets/"
+SRC_DIR = "/app/src/"
+TEMPLATES_DIR = "/app/templates/"
 
 # print Hello World
 print("Hello World")
+
+template="Minimalist Line-based Economic News Intro"
+
+template_folder = TEMPLATES_DIR + template + "/"
+mp4_file = [f for f in os.listdir(template_folder) if f.endswith('.mp4')][0]
+print(mp4_file)
+
