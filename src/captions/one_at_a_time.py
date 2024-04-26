11afe02b1105041a580574435b767bde402a82eb
Your Name <you@example.com>
2024-04-26 13:28:37 +0000

    Update Dockerfile to use Python 3.10 and install system dependencies

--- src/captions/one_at_a_time.py
+++ src/captions/one_at_a_time.py
@@ -18,19 +18,22 @@ def one_word_at_a_time(subtitle_dir, video_path):
     for line in parsed:
         print(line)
         part = line['part']
-        start = line['start']
-        end = line['end']
+        start = line['start'] / 1000
+        end = line['end'] / 1000
         # magick -list font
-        image_of_styled_text = moviepy.TextClip(part, fontsize=70, color='white', bg_color='black', font="DejaVu-Sans-Bold")
+        image_of_styled_text = moviepy.TextClip(part, fontsize=70, color='white', font="DejaVu-Sans-Bold")
         image_of_styled_text = image_of_styled_text.set_position('center')
         image_of_styled_text = image_of_styled_text.set_start(start)
         image_of_styled_text = image_of_styled_text.set_end(end)
         image_of_styled_text = image_of_styled_text.set_duration(end-start)
+        print("Start: ", start)
+        print("End: ", end)
         image_of_styled_text = image_of_styled_text.set_fps(30)
         clips.append(image_of_styled_text)
-        # video_path
-    print(parsed)
-    video_clip = moviepy.concatenate_videoclips(clips)
+    # print(parsed)
+    # exit()
+    video_clip = moviepy.CompositeVideoClip(clips)
+    # video_clip = moviepy.concatenate_videoclips(clips)
     video_clip = video_clip.set_fps(30)
     video_clip.write_videofile(filename=output_folder + "output-one-word-at-a-time.mp4", codec="libx264", audio_codec="aac")
     video_clip.close()
