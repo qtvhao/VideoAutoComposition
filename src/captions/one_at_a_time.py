de296e946c84e17a455a70f08cff9311ba8cef15
Your Name <you@example.com>
2024-04-26 15:14:38 +0000

    Refactor one_word_at_a_time function in one_at_a_time.py to improve text styling and handling of quotes

--- src/captions/one_at_a_time.py
+++ src/captions/one_at_a_time.py
@@ -17,22 +17,27 @@ def one_word_at_a_time(subtitle_dir, video_path):
     ]
     reduced_text = ""
     marker_for_same_line = False
+    marker_start = 0
+    # marker_end = 0
     for line in parsed:
         print(line)
         part = line['part']
-    # AttributeError: 'str' object has no attribute 'includes', so I will use 'in' instead, Example: 'a' in 'abc'
-        # part_is_should_be_on_same_line = part.includes('"') or part.includes("'") or part.includes("(") or part.includes("[") or part.includes("{") or part.includes("<")
+        start = line['start'] / 1000
+        end = line['end'] / 1000
         part_is_should_be_on_same_line = '"' in part or "'" in part or "(" in part or "[" in part or "{" in part or "<" in part
         if part_is_should_be_on_same_line and not marker_for_same_line:
             print("Part is should be on same line")
             reduced_text += part
+            marker_start = start
             marker_for_same_line = True
             continue
         part_is_should_be_on_new_line = '"' in part or "'" in part or ")" in part or "]" in part or "}" in part or ">" in part
-        # part_is_should_be_on_new_line = part.includes('"') or part.includes("'") or part.includes(")") or part.includes("]") or part.includes("}") or part.includes(">")
         if part_is_should_be_on_new_line:
             print("Part is should be on new line")
             marker_for_same_line = False
+            # marker_end = end
+            start = marker_start
+            # end = marker_end
             part = reduced_text + " " + part
             reduced_text = ""
         else:
@@ -40,8 +45,6 @@ def one_word_at_a_time(subtitle_dir, video_path):
                 print("Part is should be on same line")
                 reduced_text = reduced_text + " " + part
                 continue
-        start = line['start'] / 1000
-        end = line['end'] / 1000
         text_clip_params = {
             "txt": part,
             "fontsize": 70,
@@ -57,18 +60,12 @@ def one_word_at_a_time(subtitle_dir, video_path):
         }
         clip_info = {k: text_clip_params[k] for k in ('txt', 'fontsize', 'font', 'color', 'stroke_width', 'stroke_color', 'size', 'kerning', 'method', 'align') if k in text_clip_params}
         image_of_styled_text = moviepy.TextClip(**clip_info)
-        # image_of_styled_text = image_of_styled_text.set_position('center')
         image_of_styled_text = image_of_styled_text.set_start(start)
         image_of_styled_text = image_of_styled_text.set_end(end)
         image_of_styled_text = image_of_styled_text.set_duration(end-start)
-        # print("Start: ", start)
-        # print("End: ", end)
         image_of_styled_text = image_of_styled_text.set_fps(30)
         clips.append(image_of_styled_text)
-    # print(parsed)
-    # exit()
     video_clip = moviepy.CompositeVideoClip(clips)
-    # video_clip = moviepy.concatenate_videoclips(clips)
     video_clip = video_clip.set_fps(30)
     video_clip.write_videofile(filename=output_folder + "output-one-word-at-a-time.mp4", codec="libx264", audio_codec="aac")
     video_clip.close()
