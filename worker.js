70dfb3b98c341389fc4c06ba3e676df2205fce48
Your Name <you@example.com>
2024-04-27 07:36:34 +0000

    Refactor Dockerfile and main.js, and remove unnecessary files

--- worker.js
+++ worker.js
@@ -1,12 +1,54 @@
 let Queue = require('bull');
+let fs = require('fs');
+let { spawn } = require('child_process');
+const path = require('path');
 
-
+let assetsImageDir = path.join(__dirname, 'assets', 'image');
 let queue = new Queue('video-auto-composition', 'redis://redis:6379');
 
+
+queue.add({
+    compositeEngine: 'simple',
+    assets: {
+        images: fs.readdirSync(assetsImageDir).map(file => path.join(assetsImageDir, file)),
+    }
+});
+setTimeout(r => {
+    process.exit();
+}, 10_000);
 queue.process(async (job) => {
     console.log('Processing job', job.id);
     console.log('Job data', job.data);
     fs.writeFileSync('/tmp/job.json', JSON.stringify(job.data));
+    let compositeEngine = job.data.compositeEngine;
+    if (compositeEngine) {
+        console.log('Composite engine', compositeEngine);
+    }
+    if (fs.existsSync('/tmp/returnvalue.json')) {
+        fs.unlinkSync('/tmp/returnvalue.json');
+    }
+    // 
+    let script = 'app.py'
+    await new Promise((resolve, reject) => {
+        let process = spawn('python3', [script, 'composite', compositeEngine]);
+        process.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+        process.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+        process.on('close', (code) => {
+            if (code !== 0) {
+                console.error(`child process exited with code ${code}`);
+                reject();
+            }
+            console.log(`child process exited with code ${code}`);
+            resolve();
+        });
+    });
+    //
+    let returnValue = JSON.parse(fs.readFileSync('/tmp/returnvalue.json'));
+    console.log('Return value', returnValue);
 
-    return 'done';
+    return returnValue;
 });
